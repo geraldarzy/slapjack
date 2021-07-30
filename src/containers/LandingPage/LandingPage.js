8960f2b3f223f1373a85d6aa719ba20370aabec6
Gerald Arzola <geraldarzola@gmail.com>
2021-07-30 01:10:00 -0400

    connect landing page to play page via button

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './LandingPage.css'
+import { useHistory } from 'react-router-dom';
 const LandingPage = () => {
+    const history = useHistory();
     return (
         <div id='homePage'>
             <h1>Welcome to SlapJack</h1>
-            <button className='hello' onClick={()=>console.log('User clicked Play from LandingPage')}>Play</button>
+            <button className='hello' onClick={()=>{
+                console.log('User clicked Play from LandingPage');
+                history.push('/play')
+            }}>Play</button>
         </div>
     )
 }
