f14648b65c7dc45b057b8d9e20544a15684c1665
Gerald Arzola <geraldarzola@gmail.com>
2021-07-30 01:17:54 -0400

    define cards

--- src/containers/PlayPage/PlayPage.js
+++ src/containers/PlayPage/PlayPage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './PlayPage.css'
 const PlayPage = () => {
+    // deck logic
+        const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ];
+        const suits = ['hearts', 'spades', 'clubs', 'diamonds'];
+        const deck = {}
+    //
     return (
         <>
-            
+
         </>
     )
 }
 
-export default PlayPage
+export default PlayPage;
